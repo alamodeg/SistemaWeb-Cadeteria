d3bb1794bea8bf6e3f07c80ad26d9b90762a296b
Alvaro Deguer <alvarodeguer@gmail.com>
2021-09-16 21:43:36 -0300

    Agregada vista pedidos y terminado alta pedidos con menu de cadetes

---
+++ Modelo/Entidades/Cadeteria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadeteria.Model
+{ 
+    public class Cadeteria
+    {
+        string nombre;
+        List<Cadete> listadoCadetes;
+
+        public Cadeteria(string nombre, List<Cadete> listadoCadetes)
+        {
+            this.Nombre = nombre;
+            this.ListadoCadetes = listadoCadetes;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        internal List<Cadete> ListadoCadetes { get => listadoCadetes; set => listadoCadetes = value; }
+
+        public void AgregarCadete(Cadete cad)
+        {
+            listadoCadetes.Add(cad);
+        }
+    }
+}
---
+++ WebAppCadeteria/Controllers/PedidoController.cs
@@ -0,0 +1,35 @@
+﻿using Cadeteria.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCadeteria.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly ILogger<PedidoController> _logger;
+        private readonly List<Cadete> _listaCadete;
+
+        public PedidoController(ILogger<PedidoController> logger, List<Cadete> listaCadete)
+        {
+            _logger = logger;
+            _listaCadete = listaCadete;
+        }
+        public IActionResult AltaPedido(string apellido, string nombre, string tel, string dir, string obs)
+        {
+            if (apellido is null && dir is null && obs is null && nombre is null && tel is null) return View(_listaCadete);
+
+            Pedido nuevoPed = new Pedido(obs,apellido,dir,tel);
+            foreach (var item in _listaCadete)
+            {
+                {
+                    item.CargarPedido(nuevoPed);
+                }
+            }
+            return View(_listaCadete);
+        }
+    }
+}
